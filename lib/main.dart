3d43c1aae319407f8c2bb6ab44b17a97de81f4ea
John Rocha <johnathanrocha@gmail.com>
2023-10-25 13:37:41 -0300

    feat: changes path provider

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ Future<void> main() async {
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: kIsWeb
         ? HydratedStorage.webStorageDirectory
-        : await getTemporaryDirectory(),
+        : await getApplicationDocumentsDirectory(),
   );
   runApp(const MyApp());
 }
