229ef352fff5c775e228515c58bdfb82b1b353d5
John Rocha <johnathanrocha@gmail.com>
2023-10-26 17:32:09 -0300

    feat: create task tile

--- lib/widgets/task_list.dart
+++ lib/widgets/task_list.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:todo_list_firebase/blocs/task_bloc/task_bloc.dart';
-import 'package:todo_list_firebase/blocs/task_bloc/task_event.dart';
 import 'package:todo_list_firebase/models/task.dart';
+import 'package:todo_list_firebase/widgets/task_tile.dart';
 
 class TaskList extends StatelessWidget {
   const TaskList({super.key, required this.tasks});
@@ -11,23 +9,14 @@ class TaskList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: tasks.length,
-      itemBuilder: (context, index) {
-        final task = tasks[index];
-        return ListTile(
-          title: Text(task.title),
-          trailing: Checkbox(
-            value: task.isDone,
-            onChanged: (value) {
-              context.read<TaskBloc>().add(UpdateTask(task: task));
-            },
-          ),
-          onLongPress: () => context.read<TaskBloc>().add(
-                DeleteTask(task: task),
-              ),
-        );
-      },
+    return Expanded(
+      child: ListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (context, index) {
+          final task = tasks[index];
+          return TaskTile(task: task);
+        },
+      ),
     );
   }
 }
---
+++ lib/widgets/task_tile.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_list_firebase/blocs/task_bloc/task_bloc.dart';
+import 'package:todo_list_firebase/models/task.dart';
+
+class TaskTile extends StatelessWidget {
+  const TaskTile({super.key, required this.task});
+
+  final Task task;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        task.title,
+        style: TextStyle(
+          decoration: task.isDone! ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: Checkbox(
+        value: task.isDone,
+        onChanged: (value) {
+          context.read<TaskBloc>().add(UpdateTask(task: task));
+        },
+      ),
+      onLongPress: () => context.read<TaskBloc>().add(
+            DeleteTask(task: task),
+          ),
+    );
+  }
+}
