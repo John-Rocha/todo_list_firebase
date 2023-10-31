bf3fdd8a2333eb1f56cb76379c144088c507aca0
John Rocha <johnathanrocha@gmail.com>
2023-10-30 21:53:55 -0300

    refactor: task tile

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:todo_list_firebase/blocs/task_bloc/task_bloc.dart';
 import 'package:todo_list_firebase/models/task.dart';
+import 'package:todo_list_firebase/widgets/popup_menu.dart';
 
 class TaskTile extends StatelessWidget {
   const TaskTile({super.key, required this.task});
@@ -16,36 +18,77 @@ class TaskTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(
-        task.title,
-        overflow: TextOverflow.ellipsis,
-        style: TextStyle(
-          decoration: task.isDone! ? TextDecoration.lineThrough : null,
-        ),
-      ),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
-      ),
-      trailing: Row(
-        mainAxisSize: MainAxisSize.min,
+    return Padding(
+      padding: const EdgeInsets.only(left: 12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          IconButton(
-            onPressed: () =>
-                context.read<TaskBloc>().add(FavoriteTask(task: task)),
-            icon: const Icon(Icons.favorite),
+          Expanded(
+            child: Row(
+              children: [
+                const Icon(Icons.star_outline),
+                const SizedBox(width: 12),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        task.title,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 18,
+                          decoration:
+                              task.isDone! ? TextDecoration.lineThrough : null,
+                        ),
+                      ),
+                      Text(DateFormat('dd/MM/yyyy hh:mm')
+                          .format(DateTime.now())),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-          Checkbox(
-            value: task.isDone,
-            onChanged: task.isDeleted == false
-                ? (value) {
-                    context.read<TaskBloc>().add(UpdateTask(task: task));
-                  }
-                : null,
+          Row(
+            children: [
+              Checkbox(
+                value: task.isDone,
+                onChanged: task.isDeleted == false
+                    ? (value) {
+                        context.read<TaskBloc>().add(UpdateTask(task: task));
+                      }
+                    : null,
+              ),
+              PopupMenu(
+                task: task,
+                onTap: () => _removeOrDeleteTask(context, task),
+              ),
+            ],
           ),
         ],
       ),
-      onLongPress: () => _removeOrDeleteTask(context, task),
     );
   }
 }
+
+// ListTile(
+//       title: Text(
+//         task.title,
+//         overflow: TextOverflow.ellipsis,
+//         style: TextStyle(
+//           decoration: task.isDone! ? TextDecoration.lineThrough : null,
+//         ),
+//       ),
+//       shape: RoundedRectangleBorder(
+//         borderRadius: BorderRadius.circular(10),
+//       ),
+//       trailing: Checkbox(
+//         value: task.isDone,
+//         onChanged: task.isDeleted == false
+//             ? (value) {
+//                 context.read<TaskBloc>().add(UpdateTask(task: task));
+//               }
+//             : null,
+//       ),
+//       onLongPress: () => _removeOrDeleteTask(context, task),
+//     );
